3ebb5c416cb8cca7df2e78cc6e95b3b9b4edc85e
Tatiana Kosarevskaya <tv.kosarevskaya@gmail.com>
2021-07-25 16:35:00 +0300

    Initial commit

---
+++ src/test/java/ru/netology/domain/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class PostmanEchoTest {
+    @Test
+    void shouldSendData() {
+        // Given - When - Then
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some test data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some test data"));
+    }
+}
