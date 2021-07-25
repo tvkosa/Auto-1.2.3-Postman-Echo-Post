a52b84ecdc0e2c27f242f65c4bfd125f9fea7fca
Tatiana Kosarevskaya <tv.kosarevskaya@gmail.com>
2021-07-25 16:48:38 +0300

    BUILD FAILED

--- src/test/java/ru/netology/domain/PostmanEchoTest.java
+++ src/test/java/ru/netology/domain/PostmanEchoTest.java
@@ -17,6 +17,6 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some test data"));
+                .body("data", equalTo("some value"));
     }
 }
